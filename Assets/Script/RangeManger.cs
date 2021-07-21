e752502b3449d4c5dfeb468d639533df01e3b54a
uchuru <woo.churu@gmail.com>
2021-07-22 00:53:34 +0900

    .

--- Assets/Script/RangeManger.cs
+++ Assets/Script/RangeManger.cs
@@ -22,205 +22,120 @@ public class RangeManger : MonoBehaviour
         Transform rect = hitobj.parent.GetComponent<Transform>();
         MakeSoldier soldier = hitobj.GetComponent<MakeSoldier>();
 
-        
-        for (int i = 0; i < tileManger.tileList.Count; i++)
+        int nameOfLand = Int32.Parse(hitobj.parent.parent.name) - 1;
+
+        // 오른쪽 
+        if (hitobj.GetComponent<MakeSoldier>().Movement >= 1)
         {
-            // 오른쪽 
-            if (tileManger.tileList[i].parent.name == (Int32.Parse(hitobj.parent.parent.name) + 1).ToString())
+            for (int j = 0; j < hitobj.GetComponent<MakeSoldier>().Movement; j++)
             {
-                if (Int32.Parse(hitobj.parent.parent.name) % 17 != 0 
-                    && tileManger.tileList[i].gameObject.activeSelf == true
-                    && tileManger.tileList[i].GetComponent<MakeArea>().Movement)
+                //오
+                if (nameOfLand % 17 != 0
+                    && tileManger.tileList[nameOfLand + (1 + j)].gameObject.activeSelf == true
+                    && tileManger.tileList[nameOfLand + (1 + j)].GetComponent<MakeArea>().Movement)
                 {
-                    if (tileManger.tileList[i].childCount == 0)
+                    if (tileManger.tileList[nameOfLand + (1 + j)].childCount == 0)
                     {
-                        //tileManger.activeChildtileList[i].transform.GetChild(0).GetComponent<AreaManger>().pureTag = tileManger.activeChildtileList[i].transform.tag;
-                        tileManger.tileList[i].GetComponent<SpriteRenderer>().color = Color.cyan;
-                        tileManger.tileList[i].tag = "SelectLand";
-                        rangeList.Add(tileManger.tileList[i]);
-
-                        if (hitobj.GetComponent<MakeSoldier>().Movement >= 2)
-                        {
-                            for (int j = 1; j < hitobj.GetComponent<MakeSoldier>().Movement; j++)
-                            {
-                                int nameOfLand = Int32.Parse(tileManger.tileList[i].parent.name) - 1;
-
-                                if (nameOfLand % 17 != 0
-                                    && tileManger.tileList[nameOfLand + j].gameObject.activeSelf == true
-                                    && tileManger.tileList[nameOfLand + j].GetComponent<MakeArea>().Movement)
-                                {
-                                    if (tileManger.tileList[nameOfLand + j].childCount == 0)
-                                    {
-                                        //Debug.Log(tileManger.tileList[nameOfLand + j].parent.name);
-                                        tileManger.tileList[nameOfLand + j].GetComponent<SpriteRenderer>().color = Color.cyan;
-                                        tileManger.tileList[nameOfLand + j].tag = "SelectLand";
-                                        rangeList.Add(tileManger.tileList[nameOfLand + j]);
-                                    }
-                                }
-                            }
-                        }
+                        tileManger.tileList[nameOfLand + (1 + j)].GetComponent<SpriteRenderer>().color = Color.cyan;
+                        tileManger.tileList[nameOfLand + (1 + j)].tag = "SelectLand";
+                        rangeList.Add(tileManger.tileList[nameOfLand + (1 + j)]);
                     }
                     else
                     {
-                        if(tileManger.tileList[i].GetChild(0).transform.tag == "Enemy" || tileManger.tileList[i].GetChild(0).transform.tag == "GD")
+                        if (tileManger.tileList[nameOfLand + (1 + j)].GetChild(0).transform.tag == "Enemy"
+                            || tileManger.tileList[nameOfLand + (1 + j)].GetChild(0).transform.tag == "GD")
                         {
-                            tileManger.tileList[i].GetComponent<SpriteRenderer>().color = Color.red;
-                            tileManger.tileList[i].tag = "SelectLand";
-                            rangeList.Add(tileManger.tileList[i]);
+                            tileManger.tileList[nameOfLand + (1 + j)].GetComponent<SpriteRenderer>().color = Color.red;
+                            tileManger.tileList[nameOfLand + (1 + j)].tag = "SelectLand";
+                            rangeList.Add(tileManger.tileList[nameOfLand + (1 + j)]);
+                            break;
                         }
-                        
                     }
                 }
             }
 
-            //왼쪽
-            if (tileManger.tileList[i].parent.name == (Int32.Parse(hitobj.parent.parent.name) - 1).ToString())
+            for (int j = 0; j < hitobj.GetComponent<MakeSoldier>().Movement; j++)
             {
-                if (Int32.Parse(hitobj.parent.parent.name) % 17 != 0
-                    && tileManger.tileList[i].gameObject.activeSelf == true
-                    && tileManger.tileList[i].GetComponent<MakeArea>().Movement)
+                //왼
+                if (nameOfLand % 17 != 0
+                    && tileManger.tileList[nameOfLand - (1 + j)].gameObject.activeSelf == true
+                    && tileManger.tileList[nameOfLand - (1 + j)].GetComponent<MakeArea>().Movement)
                 {
-                    if (tileManger.tileList[i].childCount == 0)
+                    if (tileManger.tileList[nameOfLand - (1 + j)].childCount == 0)
                     {
-                        //tileManger.activeChildtileList[i].transform.GetChild(0).GetComponent<AreaManger>().pureTag = tileManger.activeChildtileList[i].transform.tag;
-                        tileManger.tileList[i].GetComponent<SpriteRenderer>().color = Color.cyan;
-                        tileManger.tileList[i].tag = "SelectLand";
-                        rangeList.Add(tileManger.tileList[i]);
-
-                        if (hitobj.GetComponent<MakeSoldier>().Movement >= 2)
-                        {
-                            for (int j = 1; j < hitobj.GetComponent<MakeSoldier>().Movement; j++)
-                            {
-                                int nameOfLand = Int32.Parse(tileManger.tileList[i].parent.name) - 1;
-
-                                if (nameOfLand % 17 != 0
-                                    && tileManger.tileList[nameOfLand - j].gameObject.activeSelf == true
-                                    && tileManger.tileList[nameOfLand - j].GetComponent<MakeArea>().Movement)
-                                {
-                                    if (tileManger.tileList[nameOfLand - j].childCount == 0)
-                                    {
-                                        tileManger.tileList[nameOfLand - j].GetComponent<SpriteRenderer>().color = Color.cyan;
-                                        tileManger.tileList[nameOfLand - j].tag = "SelectLand";
-                                        rangeList.Add(tileManger.tileList[nameOfLand - j]);
-                                    }
-                                }
-                            }
-                        }
+                        tileManger.tileList[nameOfLand - (1 + j)].GetComponent<SpriteRenderer>().color = Color.cyan;
+                        tileManger.tileList[nameOfLand - (1 + j)].tag = "SelectLand";
+                        rangeList.Add(tileManger.tileList[nameOfLand - (1 + j)]);
                     }
                     else
                     {
-                        if (tileManger.tileList[i].GetChild(0).transform.tag == "Enemy" || tileManger.tileList[i].GetChild(0).transform.tag == "GD")
+                        if (tileManger.tileList[nameOfLand - (1 + j)].GetChild(0).transform.tag == "Enemy"
+                            || tileManger.tileList[nameOfLand - (1 + j)].GetChild(0).transform.tag == "GD")
                         {
-                            tileManger.tileList[i].GetComponent<SpriteRenderer>().color = Color.red;
-                            tileManger.tileList[i].tag = "SelectLand";
-                            rangeList.Add(tileManger.tileList[i]);
+                            tileManger.tileList[nameOfLand - (1 + j)].GetComponent<SpriteRenderer>().color = Color.red;
+                            tileManger.tileList[nameOfLand - (1 + j)].tag = "SelectLand";
+                            rangeList.Add(tileManger.tileList[nameOfLand - (1 + j)]);
+                            break;
                         }
-
                     }
                 }
-
-                
             }
 
-            //아래
-            if (tileManger.tileList[i].parent.name == (Int32.Parse(hitobj.parent.parent.name) + 17).ToString())
+            for (int j = 0; j < hitobj.GetComponent<MakeSoldier>().Movement; j++)
             {
-                if (Int32.Parse(hitobj.parent.parent.name) % 17 != 0
-                    && tileManger.tileList[i].gameObject.activeSelf == true
-                    && tileManger.tileList[i].GetComponent<MakeArea>().Movement)
+                //위
+                if (nameOfLand % 17 != 0
+                    && tileManger.tileList[nameOfLand + (17 * (1 + j))].gameObject.activeSelf == true
+                    && tileManger.tileList[nameOfLand + (17 * (1 + j))].GetComponent<MakeArea>().Movement)
                 {
-                    if (tileManger.tileList[i].childCount == 0)
+                    if (tileManger.tileList[nameOfLand + (17 * (1 + j))].childCount == 0)
                     {
-                        //tileManger.activeChildtileList[i].transform.GetChild(0).GetComponent<AreaManger>().pureTag = tileManger.activeChildtileList[i].transform.tag;
-                        tileManger.tileList[i].GetComponent<SpriteRenderer>().color = Color.cyan;
-                        tileManger.tileList[i].tag = "SelectLand";
-                        rangeList.Add(tileManger.tileList[i]);
-
-                        if (hitobj.GetComponent<MakeSoldier>().Movement >= 2)
-                        {
-                            for (int j = 1; j < hitobj.GetComponent<MakeSoldier>().Movement; j++)
-                            {
-                                int nameOfLand = Int32.Parse(tileManger.tileList[i].parent.name) - 1;
-
-                                if (nameOfLand % 17 != 0
-                                    && tileManger.tileList[nameOfLand + j * 17].gameObject.activeSelf == true
-                                    && tileManger.tileList[nameOfLand + j * 17].GetComponent<MakeArea>().Movement)
-                                {
-                                    if (tileManger.tileList[nameOfLand + j * 17].childCount == 0)
-                                    {
-                                        tileManger.tileList[nameOfLand + j * 17].GetComponent<SpriteRenderer>().color = Color.cyan;
-                                        tileManger.tileList[nameOfLand + j * 17].tag = "SelectLand";
-                                        rangeList.Add(tileManger.tileList[nameOfLand + j * 17]);
-                                    }
-                                }
-                            }
-                        }
+                        tileManger.tileList[nameOfLand + (17 * (1 + j))].GetComponent<SpriteRenderer>().color = Color.cyan;
+                        tileManger.tileList[nameOfLand + (17 * (1 + j))].tag = "SelectLand";
+                        rangeList.Add(tileManger.tileList[nameOfLand + (17 * (1 + j))]);
                     }
                     else
                     {
-                        if (tileManger.tileList[i].GetChild(0).transform.tag == "Enemy" || tileManger.tileList[i].GetChild(0).transform.tag == "GD")
+                        if (tileManger.tileList[nameOfLand + (17 * (1 + j))].GetChild(0).transform.tag == "Enemy"
+                            || tileManger.tileList[nameOfLand + (17 * (1 + j))].GetChild(0).transform.tag == "GD")
                         {
-                            tileManger.tileList[i].GetComponent<SpriteRenderer>().color = Color.red;
-                            tileManger.tileList[i].tag = "SelectLand";
-                            rangeList.Add(tileManger.tileList[i]);
+                            tileManger.tileList[nameOfLand + (17 * (1 + j))].GetComponent<SpriteRenderer>().color = Color.red;
+                            tileManger.tileList[nameOfLand + (17 * (1 + j))].tag = "SelectLand";
+                            rangeList.Add(tileManger.tileList[nameOfLand + (17 * (1 + j))]);
+                            break;
                         }
-
                     }
                 }
-
-                
             }
 
-            //위
-            if (tileManger.tileList[i].parent.name == (Int32.Parse(hitobj.parent.parent.name) - 17).ToString())
+            for (int j = 0; j < hitobj.GetComponent<MakeSoldier>().Movement; j++)
             {
-                if (Int32.Parse(hitobj.parent.parent.name) % 17 != 0
-                    && tileManger.tileList[i].gameObject.activeSelf == true
-                    && tileManger.tileList[i].GetComponent<MakeArea>().Movement)
+                //아래
+                if (nameOfLand % 17 != 0
+                    && tileManger.tileList[nameOfLand - (17 * (1 + j))].gameObject.activeSelf == true
+                    && tileManger.tileList[nameOfLand - (17 * (1 + j))].GetComponent<MakeArea>().Movement)
                 {
-                    if (tileManger.tileList[i].childCount == 0)
+                    if (tileManger.tileList[nameOfLand - (17 * (1 + j))].childCount == 0)
                     {
-                        //tileManger.activeChildtileList[i].transform.GetChild(0).GetComponent<AreaManger>().pureTag = tileManger.activeChildtileList[i].transform.tag;
-                        tileManger.tileList[i].GetComponent<SpriteRenderer>().color = Color.cyan;
-                        tileManger.tileList[i].tag = "SelectLand";
-                        rangeList.Add(tileManger.tileList[i]);
-
-                        if (hitobj.GetComponent<MakeSoldier>().Movement >= 2)
-                        {
-                            for (int j = 1; j < hitobj.GetComponent<MakeSoldier>().Movement; j++)
-                            {
-                                int nameOfLand = Int32.Parse(tileManger.tileList[i].parent.name) - 1;
-
-                                if (nameOfLand % 17 != 0
-                                    && tileManger.tileList[nameOfLand - j * 17].gameObject.activeSelf == true
-                                    && tileManger.tileList[nameOfLand - j * 17].GetComponent<MakeArea>().Movement)
-                                {
-                                    if (tileManger.tileList[nameOfLand - j * 17].childCount == 0)
-                                    {
-                                        tileManger.tileList[nameOfLand - j * 17].GetComponent<SpriteRenderer>().color = Color.cyan;
-                                        tileManger.tileList[nameOfLand - j * 17].tag = "SelectLand";
-                                        rangeList.Add(tileManger.tileList[nameOfLand - j * 17]);
-                                    }
-                                }
-                            }
-                        }
+                        tileManger.tileList[nameOfLand - (17 * (1 + j))].GetComponent<SpriteRenderer>().color = Color.cyan;
+                        tileManger.tileList[nameOfLand - (17 * (1 + j))].tag = "SelectLand";
+                        rangeList.Add(tileManger.tileList[nameOfLand - (17 * (1 + j))]);
                     }
                     else
                     {
-                        if (tileManger.tileList[i].GetChild(0).transform.tag == "Enemy" || tileManger.tileList[i].GetChild(0).transform.tag == "GD")
+                        if (tileManger.tileList[nameOfLand - (17 * (1 + j))].GetChild(0).transform.tag == "Enemy"
+                            || tileManger.tileList[nameOfLand - (17 * (1 + j))].GetChild(0).transform.tag == "GD")
                         {
-                            tileManger.tileList[i].GetComponent<SpriteRenderer>().color = Color.red;
-                            tileManger.tileList[i].tag = "SelectLand";
-                            rangeList.Add(tileManger.tileList[i]);
+                            tileManger.tileList[nameOfLand - (17 * (1 + j))].GetComponent<SpriteRenderer>().color = Color.red;
+                            tileManger.tileList[nameOfLand - (17 * (1 + j))].tag = "SelectLand";
+                            rangeList.Add(tileManger.tileList[nameOfLand - (17 * (1 + j))]);
+                            break;
                         }
-
                     }
                 }
-
-                
             }
         }
-        
+
         input.army = hitobj;
     }
 }
