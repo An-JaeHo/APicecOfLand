4ec5b01eb8543f76101a459637be8a19bff2d541
AnJaeHo <era52001@gmail.com>
2022-02-06 16:22:53 +0900

    02 06 로그인 오류
    아무래도 gpgs를 사용한 로그인이 안되는거 같아 내일부터는 파이어베이스 로그인을 구현해 보는 방향으로 해야할 것 같습니다.

--- Assets/GooglePlayGames/GameInfo.cs
+++ Assets/GooglePlayGames/GameInfo.cs
@@ -36,9 +36,9 @@ namespace GooglePlayGames {
         private const string UnescapedWebClientId = "WEB_CLIENTID";
         private const string UnescapedNearbyServiceId = "NEARBY_SERVICE_ID";
 
-        public const string ApplicationId = "983913646573"; // Filled in automatically
+        public const string ApplicationId = "269466304542"; // Filled in automatically
         public const string IosClientId = "__IOS_CLIENTID__"; // Filled in automatically
-        public const string WebClientId = "983913646573-1sc6ltkfeiemggvj8ob4bp25aigf0hm6.apps.googleusercontent.com"; // Filled in automatically
+        public const string WebClientId = "269466304542-r5mbi88uhgg8agsvk78shn7jaqlpghhg.apps.googleusercontent.com"; // Filled in automatically
         public const string NearbyConnectionServiceId = "";
 
         public static bool ApplicationIdInitialized() {
